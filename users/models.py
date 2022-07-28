bb6e2548b9dc5442c1cefde798768a8cd1242d04
VincentGichohi <vincentegichohi@gmail.com>
2022-07-28 14:29:26 +0300

    Add a custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+    )
+    is_active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)  # an admin user; non-superuser
+    admin = models.BooleanField(default=False)  # a superuser
+
+    # notice the absence of the "password field", which is built in
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # Email and Password are required by default
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
