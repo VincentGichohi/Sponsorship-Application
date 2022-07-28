1d1def9787b4259db9ffaf04ba0b4a5517bcbe5d
VincentGichohi <vincentegichohi@gmail.com>
2022-07-28 16:24:14 +0300

    Change the import to the baseusermanager

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-from users.utils.manage import UserManager
+from .utils.manage import UserManager
 
 ALLOWED_STATUS = [
     ("ACTIVE", "ACTIVE"),
