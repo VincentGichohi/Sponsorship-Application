6dffe3ad260dd2dffe20d082bf403d7b563dbd59
VincentGichohi <vincentegichohi@gmail.com>
2022-07-28 16:53:05 +0300

    Add get user model to forms

--- users/forms.py
+++ users/forms.py
@@ -3,3 +3,6 @@ from .models import MyUser
 from .utils.manage import UserManager
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+MyUser = get_user_model()
+
