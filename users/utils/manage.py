a59dda0a26601a99d77c93a0d60d597eb7f06dca
VincentGichohi <vincentegichohi@gmail.com>
2022-07-29 23:07:38 +0300

    Modify the user manager

--- users/utils/manage.py
+++ users/utils/manage.py
@@ -1,45 +1,54 @@
-# from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
-# from . import models as user_models
+
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, username, email, is_staff, password=None):
         """
-        Creates and saves a User with the given email and password.
+        Creates a user with a given email and password
         """
+        if not username:
+            raise ValueError("Enter a valid username")
         if not email:
-            raise ValueError('Users must have an email address')
-
+            raise ValueError("Users must have an email address")
         user = self.model(
-            email=self.normalize_email(email),
+            username=username,
+            email=email,
+            is_staff=True
         )
-
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_staffuser(self, email, password):
+    def create_staffuser(self, username, email, password=None):
         """
-        Creates and saves a staff user with the given email and password.
+        Creates and saves a staff user with a given email and password
         """
-        user = self.create_user(
-            email,
-            password=password,
+        if not username:
+            raise ValueError("Users must have a valid username")
+        if not email:
+            raise ValueError("Users must have a valid email address")
+        user = self.model(
+            username=username,
+            email=email,
+            is_staff=True
         )
-        user.staff = True
+        user.set_passwod(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, username, email, password=None):
         """
-        Creates and saves a superuser with the given email and password.
+        Creates and saves a superuser with the given email and password
         """
         user = self.create_user(
-            email,
-            password=password,
+            username=username,
+            email=email,
+            is_staff=True,
+            password=password
         )
-        user.staff = True
-        user.admin = True
+        user.is_admin = True
+        user.is_active = True
         user.save(using=self._db)
+
         return user
 
