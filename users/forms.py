b309cd4c111f2e397e57044fb4f38e3ca67eb647
VincentGichohi <vincentegichohi@gmail.com>
2022-07-29 11:33:07 +0300

    Add a save method

--- users/forms.py
+++ users/forms.py
@@ -62,4 +62,12 @@ class UserAdminCreationForm(forms.ModelForm):
                 self.add_error("password_2", "Your passwords do not match")
             return cleaned_data
 
+        def save(self, commit=True):
+            # Save the provided password in a hashed format.
+            user = super().save(commit=False)
+            user.set_password(self.cleaned_data['password'])
+            if commit:
+                user.save()
+            return user
+
 
