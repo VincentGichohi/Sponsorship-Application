8aff0809a48fccaeae8af613dd9c40376382b771
VincentGichohi <vincentegichohi@gmail.com>
2022-07-29 10:50:48 +0300

    Add a clean email method

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from .models import MyUser
+from . import models as user_models
 from .utils.manage import UserManager
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-MyUser = get_user_model()
+user_models.MyUser = get_user_model()
 
 
 class RegisterForm(forms.ModelForm):
@@ -15,7 +15,7 @@ class RegisterForm(forms.ModelForm):
     password_2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
 
     class Meta:
-        model = MyUser
+        model = user_models.MyUser
         fields = ['email']
 
         def clean_email(self):
@@ -23,4 +23,9 @@ class RegisterForm(forms.ModelForm):
             Verify email is available.
             """
             email = self.cleaned_data.get('email')
-            qs = MyUser.objects.filter(email=email)
+            qs = user_models.MyUser.objects.filter(email=email)
+            if qs.exists():
+                raise forms.ValidationError("email is taken")
+            return email
+
+
