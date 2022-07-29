cc454142a550bbe1fd29bb563d766f0298c87560
VincentGichohi <vincentegichohi@gmail.com>
2022-07-29 10:34:43 +0300

    Add a register form

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,21 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 MyUser = get_user_model()
 
+
+class RegisterForm(forms.ModelForm):
+    """
+    The default
+    """
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = MyUser
+        fields = ['email']
+
+        def clean_email(self):
+            """
+            Verify email is available.
+            """
+            email = self.cleaned_data.get('email')
+            qs = MyUser.objects.filter(email=email)
