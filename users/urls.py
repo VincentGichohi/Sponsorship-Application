e850267b79c9ade11f05240c4f497638984dcf56
VincentGichohi <vincentegichohi@gmail.com>
2022-08-09 21:14:23 +0300

    Modify the urls

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from users import views as user_views
 from django.urls import path
 
 urlpatterns = [
-    path('', user_views.BaseTemplateView.as_view()),
+    path('base/', user_views.BaseTemplateView.as_view()),
     path('register/', user_views.SignUpView.as_view()),
 
 ]
