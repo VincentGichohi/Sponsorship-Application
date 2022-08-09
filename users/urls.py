f2d283f24623af5ced8977f28d5589664dd9419f
VincentGichohi <vincentegichohi@gmail.com>
2022-08-09 23:21:12 +0300

    link index view to urls

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('base/', user_views.BaseTemplateView.as_view()),
     path('register/', user_views.SignUpView.as_view()),
-
+    path('index/', user_views.IndexTemplateView.as_view()),
 ]
