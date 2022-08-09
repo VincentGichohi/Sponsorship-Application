9ec25f32b7da27d5c97dfdbe970c444df88a270e
VincentGichohi <vincentegichohi@gmail.com>
2022-08-09 19:13:27 +0300

    modify app urls

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from users import views as user_views
 from django.urls import path
 
 urlpatterns = [
-    path('', user_views.SignUpView.as_view()),
+    path('register/', user_views.SignUpView.as_view()),
 ]
