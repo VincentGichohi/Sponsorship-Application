2add9c044fdf37ebb88c33cd568e60f3da907f23
VincentGichohi <vincentegichohi@gmail.com>
2022-08-09 19:21:16 +0300

    Add template import

--- users/views.py
+++ users/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from users import models as user_models
 from users import forms as user_forms
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+
 
 class SignUpView(SuccessMessageMixin, CreateView):
     template_name = 'register.html'
     success_url = reverse_lazy('login')
     form_class = user_forms.RegisterForm
     success_message = 'Your account has been created successfully.'
-
